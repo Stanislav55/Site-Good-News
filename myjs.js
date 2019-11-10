838272560a50552d7d5e6ee620fe573fbd2af540
Stas <info7762@gmail.com>
2019-11-10 23:10:34 +0600

    Final version

--- myjs.js
+++ myjs.js
@@ -1,6 +1,6 @@
-// Слайдер для рейтига ньюзмейкеров
+// Слайдер для Good Новостей
 $('.center').slick({
-    centerMode: true,
+    centerMode: false,
     slidesToShow: 3,
     slidesToScroll: 1,
     autoplay: true,
@@ -24,7 +24,7 @@ $('.center').slick({
     ]
 });
 
-// Слайдер для видеоновостей
+// Слайдер для Видеоновостей 
 $('.lazy').slick({
     lazyLoad: 'ondemand',
     centerMode: false,
@@ -48,6 +48,33 @@ $('.lazy').slick({
                 centerMode: true,
                 centerPadding: '40px',
                 slidesToShow: 1
+            }
+        }
+    ]
+});
+
+// Слайдер для Ньюзмейкеров
+$('.multiple-items').slick({
+    centerMode: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    dots: true,
+    responsive: [
+        {breakpoint: 768,
+        settings: {
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 2
+            }
+        },
+        {breakpoint: 480,
+        settings: {
+            centerMode: true,
+            centerPadding: '40px',
+            slidesToShow: 1
             }
         }
     ]
